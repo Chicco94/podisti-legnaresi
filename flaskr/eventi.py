624b13839fcb7c25ddac926a815865442c1b6e40
Chicco94 <enrico.cominato@gmail.com>
2024-12-08 17:26:43 +0100

    impostata prenotazioni

--- flaskr/eventi.py
+++ flaskr/eventi.py
@@ -10,9 +10,12 @@ bp = Blueprint('eventi', __name__)
 def index():
     db = get_db()
     eventi = db.execute(
-        'SELECT p.id, title, body, created, author_id, username, event_date'
-        ' FROM eventi p JOIN utenti u ON p.author_id = u.id'
-        ' ORDER BY event_date DESC'
+        'SELECT e.id, title, body, created, author_id, username, event_date,ifnull(biglietti_prenotati,0) as biglietti_prenotati '
+        'FROM eventi e ' 
+        'JOIN utenti u ON e.author_id = u.id '
+        'LEFT JOIN utenti_eventi u ON id_evento = e.id AND id_utente = ? '
+        'ORDER BY event_date DESC ',
+        (str(g.user['id']) if g.user else ' ')
     ).fetchall()
     return render_template('eventi/index.html', eventi=eventi)
 
@@ -61,6 +64,28 @@ def get_evento(id, check_author=True):
     return evento
 
 
+
+def get_prenotazione(id_evento,id_utente):
+    prenotazione = get_db().execute(
+        'SELECT id_evento,id_utente,biglietti_prenotati'
+        ' FROM utenti_eventi'
+        ' WHERE id_evento = ? and id_utente=?',
+        (id_evento,id_utente)
+    ).fetchone()
+
+    if prenotazione is None:
+        db = get_db()
+        db.execute(
+            'INSERT INTO utenti_eventi (id_evento,id_utente,biglietti_prenotati)'
+            ' VALUES (?, ?, 0)',
+            (id_evento,id_utente)
+        )
+        db.commit()
+        return get_prenotazione(id_evento,id_utente)
+
+    return prenotazione
+
+
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
@@ -88,6 +113,37 @@ def update(id):
             return redirect(url_for('eventi.index'))
 
     return render_template('eventi/update.html', evento=evento)
+
+
+@bp.route('/<int:id>/add_ticket', methods=(['GET','POST']))
+@login_required
+def add_ticket(id):
+    get_prenotazione(id,g.user['id'])
+
+    db = get_db()
+    db.execute(
+        'UPDATE utenti_eventi SET biglietti_prenotati = biglietti_prenotati+1'
+        ' WHERE id_evento = ? and id_utente=?',
+        (id, g.user['id'])
+    )
+    db.commit()
+    return redirect(url_for('eventi.index'))
+
+
+
+@bp.route('/<int:id>/remove_ticket', methods=(['GET','POST']))
+@login_required
+def remove_ticket(id):
+    get_prenotazione(id,g.user['id'])
+
+    db = get_db()
+    db.execute(
+        'UPDATE utenti_eventi SET biglietti_prenotati = biglietti_prenotati-1'
+        ' WHERE id_evento = ? and id_utente=?',
+        (id, g.user['id'])
+    )
+    db.commit()
+    return redirect(url_for('eventi.index'))
 
 
 @bp.route('/<int:id>/delete', methods=('POST',))
